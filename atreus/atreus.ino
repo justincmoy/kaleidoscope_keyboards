a31f00663e47beb00f5bf822bb43e9d29ec9b2fb
Justin Moy <justincmoy@users.noreply.github.com>
2021-11-11 10:05:05 -0700

    Add symbol layer, swap nav and symbol toggles

--- atreus/atreus.ino
+++ atreus/atreus.ino
@@ -12,6 +12,7 @@ enum {
   DVORAK,
   NAV,
   NUM,
+  SYM,
   GAME
 };
 
@@ -28,7 +29,7 @@ KEYMAPS(
        Key_F, Key_G, Key_C, Key_R, Key_L,
        Key_D, Key_H, Key_T, Key_N, Key_S,
        XXX, Key_B, Key_M, Key_W, Key_V, Key_Z,
-       LT(NAV, Enter), SFT_T(Backspace), Key_Delete, XXX, XXX, M(MACRO_LOCK_GAME)
+       LT(SYM, Enter), SFT_T(Backspace), LT(NAV, Delete), XXX, XXX, M(MACRO_LOCK_GAME)
   ),
 
   [NAV] = KEYMAP_STACKED
@@ -53,6 +54,19 @@ KEYMAPS(
 
        Key_LeftBracket, Key_Semicolon, Key_Minus, Key_Backtick, Key_RightBracket,
        Key_6, Key_7, Key_8, Key_9, Key_0,
+       ___, ___, Key_LeftShift, Key_LeftControl, Key_LeftAlt, Key_LeftGui,
+       ___, ___, ___, ___, ___, ___
+  ),
+
+  [SYM] = KEYMAP_STACKED
+  (
+       ___, LSHIFT(Key_Equals), LSHIFT(Key_Backslash), LSHIFT(Key_Semicolon), ___,
+       LSHIFT(Key_1), LSHIFT(Key_2), LSHIFT(Key_3), LSHIFT(Key_4), LSHIFT(Key_5),
+       Key_LeftGui, Key_LeftAlt, Key_LeftControl, Key_LeftShift, ___, ___,
+       ___, ___, ___, ___, ___, ___,
+
+       LSHIFT(Key_LeftBracket), Key_Semicolon, LSHIFT(Key_Minus), LSHIFT(Key_Backtick), LSHIFT(Key_RightBracket),
+       LSHIFT(Key_6), LSHIFT(Key_7), LSHIFT(Key_8), LSHIFT(Key_9), LSHIFT(Key_0),
        ___, ___, Key_LeftShift, Key_LeftControl, Key_LeftAlt, Key_LeftGui,
        ___, ___, ___, ___, ___, ___
   ),
