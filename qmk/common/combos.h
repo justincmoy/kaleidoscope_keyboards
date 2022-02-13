b743fb80d63dd8f97b7f9ed581d439ff2c60e591
Justin Moy <justincmoy@gmail.com>
2022-02-13 08:06:42 -0700

    Add combo for enter

--- qmk/common/combos.h
+++ qmk/common/combos.h
@@ -9,6 +9,8 @@ enum combos {
   COMBO_TAB,
   COMBO_BSPC,
   COMBO_DEL,
+  COMBO_ENTER_LEFT,
+  COMBO_ENTER_RIGHT,
   COMBO_1_4,
   COMBO_1_5,
 #endif
@@ -29,6 +31,8 @@ const uint16_t PROGMEM combo_esc[] = {KC_HR_0_1_0, KC_HR_0_1_1, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_HR_0_1_1, KC_HR_0_1_2, COMBO_END};
 const uint16_t PROGMEM combo_bspc[] = {KC_HR_0_1_7, KC_HR_0_1_8, COMBO_END};
 const uint16_t PROGMEM combo_del[] = {KC_HR_0_1_8, KC_HR_0_1_9, COMBO_END};
+const uint16_t PROGMEM combo_enter_left[] = {KC_HR_0_1_0, KC_HR_0_1_3, COMBO_END};
+const uint16_t PROGMEM combo_enter_right[] = {KC_HR_0_1_6, KC_HR_0_1_9, COMBO_END};
 const uint16_t PROGMEM combo_1_4[] = {KC_HR_0_1_2, KC_HR_0_1_3, COMBO_END};
 const uint16_t PROGMEM combo_1_5[] = {KC_HR_0_1_6, KC_HR_0_1_7, COMBO_END};
 #endif
@@ -47,6 +51,8 @@ combo_t key_combos[] = {
   [COMBO_TAB] = COMBO(combo_tab, KC_TAB),
   [COMBO_BSPC] = COMBO(combo_bspc, KC_BSPC),
   [COMBO_DEL] = COMBO(combo_del, KC_DEL),
+  [COMBO_ENTER_LEFT] = COMBO(combo_enter_left, KC_ENT),
+  [COMBO_ENTER_RIGHT] = COMBO(combo_enter_right, KC_ENT),
   [COMBO_1_4] = COMBO(combo_1_4, KC_OPT0_1_4),
   [COMBO_1_5] = COMBO(combo_1_5, KC_OPT0_1_5),
 #endif
