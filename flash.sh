ee38c3644975495031ec04aa521467c8981bf6bb
Justin Moy <justincmoy@gmail.com>
2022-05-02 16:59:36 -0600

    Use separate buttery engine

--- flash.sh
+++ flash.sh
@@ -12,12 +12,13 @@ if [ ! -d "$SCRIPT_DIR/qmk/$1" ]; then
 	exit 1
 fi
 
-pushd "$SCRIPT_DIR/../qmk_firmware/users/dennytom/chording_engine/"
-python parser.py "$SCRIPT_DIR/qmk/keymap_def.json" "$SCRIPT_DIR/qmk/$1/keymap.c"
+pushd "$SCRIPT_DIR/../buttery_engine/"
+python my_parser.py "$SCRIPT_DIR/qmk/keymap_def.json"
 popd
 
 rm -r "$SCRIPT_DIR/../qmk_firmware/keyboards/$1/keymaps/justincmoy"
 mkdir "$SCRIPT_DIR/../qmk_firmware/keyboards/$1/keymaps/justincmoy"
 cp -r $SCRIPT_DIR/qmk/$1/* "$SCRIPT_DIR/../qmk_firmware/keyboards/$1/keymaps/justincmoy"
 cp "$SCRIPT_DIR/qmk/common/config.h" "$SCRIPT_DIR/../qmk_firmware/keyboards/$1/keymaps/justincmoy"
+cp "$SCRIPT_DIR/../buttery_engine/keymap.c" "$SCRIPT_DIR/../qmk_firmware/keyboards/$1/keymaps/justincmoy"
 qmk flash -kb "$1" -km "justincmoy"
