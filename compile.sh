f2a10aa0d1e67a3b9670a9a4fa68a5d6ac8dd064
Justin Moy <justincmoy@gmail.com>
2023-08-21 08:53:31 -0600

    Make kaleidoscope volume read-only

--- compile.sh
+++ compile.sh
@@ -15,4 +15,4 @@ fi
 mustache values.yaml $1.mustache > kaleidoscope/$1/$1.ino
 
 docker build -q -t kaleidoscope/docker $SCRIPT_DIR/../kaleidoscope/etc
-docker run --rm -v $SCRIPT_DIR/../kaleidoscope:/kaleidoscope: -v $SCRIPT_DIR:/keymaps: --env ARDUINO_DIRECTORIES_DATA=/arduino-cli/data --env ARDUINO_DIRECTORIES_USER=/kaleidoscope/.arduino/user/ --env _NO_SYNC_KALEIDOSCOPE=1 --env KALEIDOSCOPE_DIR=/kaleidoscope kaleidoscope/docker "cp /keymaps/kaleidoscope/Makefile /keymaps/kaleidoscope/$1/ && make --directory /keymaps/kaleidoscope/$1 compile"
+docker run --rm -v $SCRIPT_DIR/../kaleidoscope:/kaleidoscope:ro -v $SCRIPT_DIR:/keymaps --env ARDUINO_DIRECTORIES_DATA=/arduino-cli/data --env ARDUINO_DIRECTORIES_USER=/kaleidoscope/.arduino/user/ --env _NO_SYNC_KALEIDOSCOPE=1 --env KALEIDOSCOPE_DIR=/kaleidoscope kaleidoscope/docker "cp /keymaps/kaleidoscope/Makefile /keymaps/kaleidoscope/$1/ && make --directory /keymaps/kaleidoscope/$1 compile"
