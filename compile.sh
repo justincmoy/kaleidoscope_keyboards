63aa88d313b3613ae1864131cbdeb19c60b84ea0
Justin Moy <justincmoy@gmail.com>
2024-04-17 21:05:58 -0600

    Use Jinja2 template, remove Cycle

--- compile.sh
+++ compile.sh
@@ -12,7 +12,11 @@ if [ "$1" = 'crkbd' ]; then
     exit
 fi
 
-mustache values.yaml $1.mustache > kaleidoscope/$1/$1.ino
+if [ "$1" = 'model100' ]; then
+    j2 -f yaml $1.j2 values.yaml --filters filters.py -o kaleidoscope/$1/$1.ino
+else
+    mustache values.yaml $1.mustache > kaleidoscope/$1/$1.ino
+fi
 
 docker build -q -t kaleidoscope/docker $SCRIPT_DIR/../kaleidoscope/etc
 docker run --rm -v $SCRIPT_DIR/../kaleidoscope:/kaleidoscope:ro -v $SCRIPT_DIR:/keymaps --env ARDUINO_DIRECTORIES_DATA=/arduino-cli/data --env ARDUINO_DIRECTORIES_USER=/kaleidoscope/.arduino/user/ --env _NO_SYNC_KALEIDOSCOPE=1 --env KALEIDOSCOPE_DIR=/kaleidoscope kaleidoscope/docker "cp /keymaps/kaleidoscope/Makefile /keymaps/kaleidoscope/$1/ && make --directory /keymaps/kaleidoscope/$1 compile"
